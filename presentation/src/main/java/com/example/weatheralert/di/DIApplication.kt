94d91f1c7aa8946c7974cdbceed84691c899cde4
Seungjin1102 <seungjin3047@gmail.com>
2023-01-25 15:12:28 +0900

    feat Timber 라이브러리 추가
    Timber Debug 모드일때만 로그 출력으로 커스텀

--- presentation/src/main/java/com/example/weatheralert/di/DIApplication.kt
+++ presentation/src/main/java/com/example/weatheralert/di/DIApplication.kt
@@ -1,8 +1,25 @@
 package com.example.weatheralert.di
 
 import android.app.Application
+import com.example.weatheralert.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class DIApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(TimberDebugTree())
+        }
+    }
+}
+
+class TimberDebugTree: Timber.DebugTree() {
+    override fun createStackElementTag(element: StackTraceElement): String? {
+        return "${element.fileName}:${element.lineNumber}#${element.methodName}"
+    }
+
 }
