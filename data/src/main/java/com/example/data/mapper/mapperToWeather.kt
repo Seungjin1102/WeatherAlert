41992b8a893fc968bdeabd7a74f971ff1f9d88a3
Seungjin1102 <seungjin3047@gmail.com>
2023-02-01 12:01:52 +0900

    feat: Entity 필드 추가
    강수형태를 알기위해 Entity PTY 필드 추가

--- data/src/main/java/com/example/data/mapper/mapperToWeather.kt
+++ data/src/main/java/com/example/data/mapper/mapperToWeather.kt
@@ -4,17 +4,17 @@ import com.example.data.model.weather.WeatherResponse
 import com.example.domain.model.WeatherEntity
 
 fun mapperToWeather(items: WeatherResponse.Items): List<WeatherEntity> {
-
     val weatherEntityList = mutableListOf<WeatherEntity>()
     val itemsList = items.item.toMutableList()
 
     while (itemsList.size >= 12) {
         val item = itemsList.subList(0, 12)
-        var weatherEntity = WeatherEntity(item[0].fcstDate, item[0].fcstTime, "", "", "")
+        val weatherEntity = WeatherEntity(item[0].fcstDate, item[0].fcstTime, "", "", "", "")
 
         item.filter { it.category == "TMP" }.map { weatherEntity.tmp = it.fcstValue }
         item.filter { it.category == "POP" }.map { weatherEntity.pop = it.fcstValue }
         item.filter { it.category == "SKY" }.map { weatherEntity.sky = it.fcstValue }
+        item.filter { it.category == "PTY" }.map { weatherEntity.pty = it.fcstValue }
 
         weatherEntityList.add(weatherEntity)
 
--- domain/src/main/java/com/example/domain/model/WeatherEntity.kt
+++ domain/src/main/java/com/example/domain/model/WeatherEntity.kt
@@ -5,5 +5,6 @@ data class WeatherEntity(
     val time: String, //시간
     var tmp: String, //기온
     var pop: String, //강수 확률
-    var sky: String //하늘 상태 코드
+    var sky: String, //하늘 상태 코드(1: 맑음, 3: 구름많음, 4: 흐림)
+    var pty: String, //강수 형태(0: 없음, 1: 비, 2: 비/눈, 3: 눈, 4: 소나기)
 )
