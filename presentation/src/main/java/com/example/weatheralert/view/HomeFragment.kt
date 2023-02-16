7042ce1ff7085ff91a1c7ac3384a5ca15364738f
Seungjin1102 <seungjin3047@gmail.com>
2023-02-16 17:51:26 +0900

    style: 불필요한 코드 제거
    테스트용 불필요한 코드 제거

--- presentation/src/main/java/com/example/weatheralert/view/HomeFragment.kt
+++ presentation/src/main/java/com/example/weatheralert/view/HomeFragment.kt
@@ -11,19 +11,11 @@ import androidx.lifecycle.repeatOnLifecycle
 import com.example.weatheralert.R
 import com.example.weatheralert.base.BaseFragment
 import com.example.weatheralert.databinding.FragmentHomeBinding
-import com.example.weatheralert.util.WeatherUtil
 import com.example.weatheralert.viewmodel.WeatherViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 import com.gun0912.tedpermission.PermissionListener
 import com.gun0912.tedpermission.normal.TedPermission
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import timber.log.Timber
-import java.text.SimpleDateFormat
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
-import java.util.*
 
 @AndroidEntryPoint
 class HomeFragment: BaseFragment<FragmentHomeBinding, WeatherViewModel>(R.layout.fragment_home) {
@@ -35,20 +27,15 @@ class HomeFragment: BaseFragment<FragmentHomeBinding, WeatherViewModel>(R.layout
         super.onViewCreated(view, savedInstanceState)
         binding.viewmodel = viewModel
 
-        viewLifecycleOwner.lifecycleScope.launch {
-            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.shortWeatherUiState.collect {
-//                    handleState(it)
-                }
-            }
-        }
-
-        binding.title.setOnClickListener {
-//            getWeather()
-        }
+//        viewLifecycleOwner.lifecycleScope.launch {
+//            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+//                viewModel.shortWeatherUiState.collect {
+//
+//                }
+//            }
+//        }
 
         requestPermission()
-//        getWeather()
     }
 
     private fun getWeather() {
@@ -65,15 +52,8 @@ class HomeFragment: BaseFragment<FragmentHomeBinding, WeatherViewModel>(R.layout
             .setPermissionListener(object : PermissionListener {
                 override fun onPermissionGranted() {
                     isCheckPermission = true
-//                    getWeather()
-//                    CoroutineScope(Dispatchers.Main).launch {
-//                        viewModel.getAddress(requireActivity())
-//                    }
                     getWeather()
-//                    viewModel.getAddress(requireActivity())
-                    Timber.d("코루틴 밖")
                 }
-
                 override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
                     Timber.d("위치 권한 없음")
                     isCheckPermission = false
@@ -83,16 +63,4 @@ class HomeFragment: BaseFragment<FragmentHomeBinding, WeatherViewModel>(R.layout
             .setPermissions(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)
             .check()
     }
-
-//    private fun handleState(uiState: UiState) = when (uiState) {
-//        is UiState.Loading -> Toast.makeText(requireContext(), "로딩 중", Toast.LENGTH_SHORT).show()
-//        is UiState.Success<*> -> {
-//           if (uiState.data is List<*>) {
-////               binding.textView.text = uiState.data.toString()
-//           } else {
-//               binding.address.text = uiState.data.toString()
-//           }
-//        }
-//        is UiState.Error -> Toast.makeText(requireContext(), "에러 발생", Toast.LENGTH_SHORT).show()
-//    }
 }
